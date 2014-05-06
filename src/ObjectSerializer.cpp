fdce35a77e3b2f24639096a3d542e35e4735e69f
Ron <ronmeir@post.bgu.ac.il>
2014-05-06 19:28:41 +0300

    working on Objects sendind

--- src/ObjectSerializer.cpp
+++ src/ObjectSerializer.cpp
@@ -6,7 +6,6 @@
  */
 
 #include "ObjectSerializer.h"
-
 ObjectSerializer::ObjectSerializer() {
 	// TODO Auto-generated constructor stub
 
@@ -18,5 +17,35 @@ ObjectSerializer::~ObjectSerializer() {
 
 
 void ObjectSerializer::getRelevantTrans(StateMachine& SM , std::vector<Transition3Tuple>& trans){
+	const std::vector<Transition3Tuple>* t =SM.translateStateMachineToTriples(); //get all the trans of the state machine in a vector
+	int size =SM.getTotalNumOfStates();
+	for(int i=0;i<size;i++){
+		int ctr=0; //count the #trans. from the i'th state
+
+		//this is a counting "for"
+		for(int j=0;i<ALPHABET_SIZE;j++){
+			Transition3Tuple tran =t->at(i*ALPHABET_SIZE+j);
+
+			//if a real trans. happens here we want to count it!
+			if(tran.get_X() != tran.get_Y()){
+				ctr++;
+				//TODO create msgTransObject and push it to "trans"
+			}//if
+		}//for (j)
+		//done with counting for the i's state s[i]
+
+		//TODO push the ctr into the "amountOfTransPerState"
+	}//for (i)
+
+
+
+
+
+
+
+
+
+
+
 
 }//end of getRelevantTrans
