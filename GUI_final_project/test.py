ea1b063ffae3b7adfe5331d07a4a83718152bdb5
Ron <ronmeir@post.bgu.ac.il>
2014-07-05 12:00:05 +0300

    print ip and port to terminal was addded

--- GUI_final_project/test.py
+++ GUI_final_project/test.py
@@ -5,7 +5,7 @@ import socket
 import easygui
 
 ############################ IP and PORT form UI ########################################
-IP='10.0.0.11' 
+IP='10.0.0.1' 
 PORT= 12345
 #########################################################################################
 
@@ -217,6 +217,8 @@ def send_was_pressed():
 #called when the "clear" button is preesed	
 def clear_was_pressed():
 	textbox.delete('1.0', 'end')
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nscreen was cleared!!!")
+
 
 #called when the "connect" button is preesed   
 def connect_was_pressed():
@@ -224,6 +226,7 @@ def connect_was_pressed():
 	insert_text("	IP  :"+str(getIP()))
 	insert_text("	PORT:"+str(getPort())+"\n")
 	
+	print("GUI server info:	IP:"+str(getIP())+"	PORT:"+str(getPort()))
 	msg=MSG_req_sm_from_server()	#get to relevant msg tuple (toSend,toShow)
 	send(msg)						#this is the actual MSG sent 
 	
@@ -262,7 +265,7 @@ def send(msg):
 	
 	
 	print("printing msg:"+str(msg))	
-	#insert_text(msg,color)
+	#insert_text(msg,color) 
 	
 	s.send(msg[0].encode())
 	#recv=str(s.recv(getReadSize()).decode('utf-8'))
