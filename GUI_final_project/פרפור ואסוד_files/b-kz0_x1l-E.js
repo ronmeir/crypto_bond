a00b5571fb854fdaa8451d0455a63658511a703e
Ron <ronmeir@post.bgu.ac.il>
2014-07-16 17:53:37 +0300

    vv

--- GUI_final_project/פרפור ואסוד_files/b-kz0_x1l-E.js
+++
@@ -1,5 +0,0 @@
-/*!CK:4087675781!*//*1404702051,*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["hlOWA"]); }
-
-__d("XPubcontentInlinePhotoPivotsEventsControllerURIBuilder",["XControllerURIBuilder"],function(a,b,c,d,e,f,g){e.exports=g.create("\/pubcontent\/inline_photo_pivots_chaining\/events\/",{});},null);
